02cdfe9619b73ac13e60ae5a2d0b96f3bcb2f870
Varun <varun.g.dsu@gmail.com>
2024-08-14 16:14:49 +0530

    Defined user model

---
+++ backend/models/user.model.js
@@ -0,0 +1,51 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    fullName: {
+        type: String,
+        required: [true, 'Name is required'],
+    },
+    userName: {
+        type: String,
+        required: [true, 'Username is required'],
+        unique: true,
+    },
+    email: {
+        type: String,
+        required: [true, 'Email is required'],
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: [true, 'Password is required']
+    },
+    followers: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+    }],
+    following: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+    }],
+    profileImage: {
+        type: String,
+        default: '',
+    },
+    coverImage: {
+        type: String,
+        default: '',
+    },
+    bio: {
+        type: String,
+        default: '',
+    },
+    link: {
+        type: String,
+        default: '',
+    }
+
+});
+
+const User = mongoose.model('User', userSchema);
+
+export default User; 
