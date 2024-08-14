1d8fcac10cd87560825b85b9f11645991b81c57f
Varun <varun.g.dsu@gmail.com>
2024-08-14 16:13:05 +0530

    Added middleware to parse request body, form data and cookies

--- backend/server.js
+++ backend/server.js
@@ -2,11 +2,16 @@ import express from "express";
 import dotenv from 'dotenv';
 import authRoutes from './routes/auth.routes.js'
 import connectToDB from "./db/connectToDb.js";
+import cookieParser from "cookie-parser";
 
 
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+app.use(express.json()); //to parse request body    
+app.use(express.urlencoded({ extended: true })); //to parse form data
+app.use(cookieParser()); //to parse cookies
 
 app.use('/api/auth', authRoutes);
 
