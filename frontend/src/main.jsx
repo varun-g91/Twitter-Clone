5741175aa6a5ea484452e937828170bfe658d1e2
Varun <varun.g.dsu@gmail.com>
2024-11-17 09:17:03 +0530

    Integrated context API into the code base to better handle states

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,11 +2,14 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter as Router } from "react-router-dom";
 import App from "./App.jsx";
+import { SignupProvider } from "./signup flow/SignupContext"; // Import your context provider
 
 createRoot(document.getElementById("root")).render(
     <StrictMode>
-        <Router>
-            <App />
-        </Router>
+        <SignupProvider>
+            <Router>
+                <App />
+            </Router>
+        </SignupProvider>
     </StrictMode>
 );
