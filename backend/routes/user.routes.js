cbee977dfef32d594f30131097d703747d97c4a4
Varun <varun.g.dsu@gmail.com>
2024-11-17 09:08:39 +0530

    Added routes for uploading profile image and to generate username suggestions

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -1,10 +1,19 @@
 import express from "express";
 import { protectRoute } from "../middleware/protectRoute.js";
-import { getUserProfile, followUnfollowUser, getSuggestedUsers, updateUser } from "../controllers/user.controller.js";
+import {
+    getUserProfile,
+    getSuggestedUsernames,
+    updateUserProfileImage,
+    followUnfollowUser,
+    getSuggestedUsers,
+    updateUser
+} from "../controllers/user.controller.js";
 
 const router = express.Router();
 
-router.get('/profile/:userName', protectRoute, getUserProfile);
+router.get('/profile/:identifier', protectRoute, getUserProfile);
+router.post('/profile/:identifier/set-profile-image', updateUserProfileImage);
+router.post('/generate-username', getSuggestedUsernames)
 router.get('/suggested', protectRoute, getSuggestedUsers);
 router.post('/follow/:id', protectRoute, followUnfollowUser);
 router.post('/update', protectRoute, updateUser);
