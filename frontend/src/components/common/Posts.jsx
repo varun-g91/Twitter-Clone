8faecf8b03000700bfd54494d2825b21ea6137bf
Varun <varun.g.dsu@gmail.com>
2024-12-10 12:15:26 +0530

    created posts component

--- frontend/src/components/common/Posts.jsx
+++ frontend/src/components/common/Posts.jsx
@@ -1,9 +1,78 @@
-import React from 'react'
+import Post from "./Post";
+import PostSkeleton from "../skeletons/PostSkeleton";
+import { useQuery } from "@tanstack/react-query";
+import { useEffect } from "react";
+import axios from "axios";
 
-const Posts = () => {
-  return (
-    <div>Posts</div>
-  )
-}
+const Posts = ({ feedType, username, userId }) => {
+    const getPostEndpoint = () => {
+        switch (feedType) {
+            case "forYou":
+                return "/api/posts/all";
+            case "following":
+                return "/api/posts/following";
+            case "posts":
+                return `/api/posts/user/${username}`;
+            case "likes":
+                return `/api/posts/likes/${userId}`;
+            default:
+                return "/api/posts/all";
+        }
+    };
 
-export default Posts
+    const POST_ENDPOINT = getPostEndpoint();
+
+    const {
+        data: posts,
+        isLoading,
+        refetch,
+        isRefetching,
+    } = useQuery({
+        queryKey: ["posts"],
+        queryFn: async () => {
+            try {
+                const response = await axios.get(POST_ENDPOINT);
+                
+                if (response.status !== 200) {
+                    throw new Error(
+                        response?.data?.message || "Something went wrong"
+                    );
+                }
+
+                return response.data?.posts;
+            } catch (error) {
+                throw new Error(error);
+            }
+        },
+    });
+
+    useEffect(() => {
+        refetch();
+    }, [feedType, refetch, username]);
+
+    return (
+        <>
+            {(isLoading || isRefetching) && (
+                <div className="flex flex-col justify-center">
+                    <PostSkeleton />
+                    <PostSkeleton />
+                    <PostSkeleton />
+                    <PostSkeleton />
+                </div>
+            )}
+            {!isLoading && !isRefetching && posts?.length === 0 && (
+                <p className="text-center my-4">
+                    No posts in this tab. Switch 👻
+                </p>
+            )}
+            {!isLoading && !isRefetching && posts && (
+                <div className="flex flex-col overflow-y-scroll h-[70vh] scrollbar-daisy">
+                    {posts.map((post) => (
+                        <Post key={post._id} post={post} />
+                    ))}
+                </div>
+            )}
+        </>
+    );
+};
+export default Posts;
